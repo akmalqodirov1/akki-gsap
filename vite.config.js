43e672366efd518d7a8fb2c304f46d7b15c3f787
akmalqodirov1 <akmalqodirov01@gmail.com>
2025-10-28 09:58:17 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: true,
+    },
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    port: 5173,
+    strictPort: true,
+  }
 })
